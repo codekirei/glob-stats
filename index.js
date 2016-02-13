4e0c0c126028cac704baabfd73f63b1a3f4a887f
Jacob Blakely <jacob@codekirei.com>
2016-02-13 12:30:29 -0800

    modularize link-target and promises

--- index.js
+++ index.js
@@ -2,50 +2,21 @@
 
 // import
 //----------------------------------------------------------
-const Promise    = require('bluebird')
-const co         = require('co')
-const fs         = require('fs')
-const globby     = require('globby')
-const isexe      = require('isexe')
-const p          = require('path')
+// npm
+const co = require('co')
+const globby = require('globby')
+const isexe = require('isexe')
 
-// promisification
-//----------------------------------------------------------
-const getStats = Promise.promisify(fs.lstat)
-const readLink = Promise.promisify(fs.readlink)
-
-// top level vars
-//----------------------------------------------------------
+// local
 
 // jsdoc
-function* linkTarget(path) {
-
-  const target = { target: yield readLink(path) }
-  const relTarget = p.resolve(p.dirname(path), target.target)
-  const stats = yield getStats(relTarget)
-
-  if (stats.isDirectory()) target.type = 'dir'
-  else if (stats.isFile())
-    target.type = (yield isexe(relTarget)) ? 'exe' : 'file'
-
-  return target
-}
-
-// jsdoc
-function* globContents(potentialGlob) {
-
-  // ensure glob and compute common root
-  //----------------------------------------------------------
-  const glob = potentialGlob.indexOf('*') >= 0
-    ? potentialGlob
-    : p.join(potentialGlob, '*')
-  const root = glob.slice(0, glob.indexOf('*'))
+function* globStats(glob, opts) {
 
   // create output skeleton
   //----------------------------------------------------------
   const out =
     { glob
-    , root
+    , root: glob.slice(0, glob.indexOf('*'))
     , contents:
       { files: {}
       , exes: {}
@@ -95,4 +66,4 @@ function* globContents(potentialGlob) {
 
 // export
 //----------------------------------------------------------
-module.exports = co.wrap(globContents)
+module.exports = co.wrap(globStats)
