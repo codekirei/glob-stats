91ce5822f268b37c02cc73c113ea1ae2cc028fe6
Jacob Blakely <jacob@codekirei.com>
2016-02-13 12:55:18 -0800

    add global test modules

---
+++ test/meta/modules.js
@@ -0,0 +1,33 @@
+'use strict'
+
+// require modules used in testing and add to global object
+// this file is injected by mocha.opts
+
+// npm
+//----------------------------------------------------------
+// chai
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
+global.assert = chai.assert
+
+// sinon
+const sinon = require('sinon')
+const clock = () => {
+  let c
+  return { freeze: () => c = sinon.useFakeTimers()
+         , tick: ms => c.tick(ms)
+         , restore: () => c.restore()
+         }
+}
+global.sinon = sinon
+global.clock = clock()
+
+// local
+//----------------------------------------------------------
+global.globStats = require('../..')
+global.age = require('../../lib/age')
+global.dirSize = require('../../lib/dir-size')
+global.linkTarget = require('../../lib/link-target')
+global.proms = require('../../lib/proms')
+global.size = require('../../lib/size')
